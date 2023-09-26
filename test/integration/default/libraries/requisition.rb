fc3f5694606dfc47c76eea3aba1baa0bfcc5a197
David Schlenk <dschlenk@convergeone.com>
2023-09-26 15:24:45 -0500

    implement existence

--- test/integration/default/libraries/requisition.rb
+++ test/integration/default/libraries/requisition.rb
@@ -10,6 +10,7 @@ class PrisRequisition < Inspec.resource(1)
 
   example '
     describe pris_requisition(\'myRouter\') do
+      it { should exist }
       its(\'source\') { should eq \'xls\' }
       its(\'source_properties\') { should eq \'file\' => \'../myInventory.xls\' }
       its(\'mapper\') { should eq \'echo\' }
@@ -17,21 +18,27 @@ class PrisRequisition < Inspec.resource(1)
     end
   '
 
+  def exists?
+    inspec.file("/opt/opennms-pris/requisitions/#{@requisition_name}/requisition.properties").file?
+  end
+
   def initialize(req_name)
     @requisition_name = req_name
-    requisition_file = "/opt/opennms-pris/requisitions/#{@requisition_name}/requisition.properties"
-    props = JavaProperties.parse(inspec.file(requisition_file).content)
-    @source = props[:source].to_s
-    @mapper = props[:mapper].to_s
-    @source_properties = {}
-    @mapper_properties = {}
-    props.each do |k, v|
-      source_properties[k.to_s[7..-1]] = v if k.to_s.start_with?('source.')
-      mapper_properties[k.to_s[7..-1]] = v if k.to_s.start_with?('mapper.')
+    if exists?
+      requisition_file = "/opt/opennms-pris/requisitions/#{@requisition_name}/requisition.properties"
+      props = JavaProperties.parse(inspec.file(requisition_file).content)
+      @source = props[:source].to_s
+      @mapper = props[:mapper].to_s
+      @source_properties = {}
+      @mapper_properties = {}
+      props.each do |k, v|
+        source_properties[k.to_s[7..-1]] = v if k.to_s.start_with?('source.')
+        mapper_properties[k.to_s[7..-1]] = v if k.to_s.start_with?('mapper.')
+      end
+      @script_file = []
+      @script_file = props[:script_file].split(',') unless props[:script_file].nil?
+      @content = RestClient.get("http://localhost:8000/requisitions/#{req_name}").body
     end
-    @script_file = []
-    @script_file = props[:script_file].split(',') unless props[:script_file].nil?
-    @content = RestClient.get("http://localhost:8000/requisitions/#{req_name}").body
   end
 
   attr_reader :requisition_name
