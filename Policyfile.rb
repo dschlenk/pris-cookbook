3904297ac3f1033565b0b0735f1b9940116e2fd2
ssgayathri <sgayatri@onec1.com>
2025-02-14 01:26:43 +0530

    Update Policyfile.rb

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,6 +1,6 @@
 name 'opennms-templates'
 default_source :supermarket
 metadata
-run_list 'opennms::postgres', ''pris_test', 'pris_java'
+run_list 'pris_test::postgres', 'pris_test', 'pris_java::user'
 cookbook 'pris_test', path: 'test/fixtures/cookbooks/pris_test'
 cookbook 'pris_java', path: 'test/fixtures/cookbooks/pris_java'
