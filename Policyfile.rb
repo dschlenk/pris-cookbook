0bcd1d6c1c495111ce4f2b0dbb80aab27ef5b316
ssgayathri <sgayatri@onec1.com>
2025-02-16 15:05:47 +0530

    Update Policyfile.rb

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,6 +1,6 @@
 name 'pris-templates'
 default_source :supermarket
 metadata
-run_list 'pris_test::postgres', 'pris_test', 'pris_java::user'
+run_list 'pris_java::default', 'pris::default', 'pris_test::file_source', 'pris_test::http_source', 'pris_test::jdbc_source', 'pris_test::merge_source', 'pris_test::script_source', 'pris_test::xls_source'
 cookbook 'pris_test', path: 'test/fixtures/cookbooks/pris_test'
 cookbook 'pris_java', path: 'test/fixtures/cookbooks/pris_java'
