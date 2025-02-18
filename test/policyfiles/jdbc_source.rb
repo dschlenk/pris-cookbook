b867af0b53c93148441bdf591bf0063c12cd9247
ssgayathri <sgayatri@onec1.com>
2025-02-19 00:18:05 +0530

    Update jdbc_source.rb

--- test/policyfiles/jdbc_source.rb
+++ test/policyfiles/jdbc_source.rb
@@ -2,5 +2,5 @@ name 'pris_jdbc_source'
 default_source :supermarket
 run_list 'pris_java::default', 'pris_test::jdbc_source'
 cookbook 'pris', path: '../..'
-cookbook 'pris_test', path: '../test/fixtures/cookbooks/pris_test'
-cookbook 'pris_java', path: '../test/fixtures/cookbooks/pris_java'
+cookbook 'pris_test', path: '../fixtures/cookbooks/pris_test'
+cookbook 'pris_java', path: '../fixtures/cookbooks/pris_java'
