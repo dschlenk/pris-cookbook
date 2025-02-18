a1547f3bb89aa4166c5940127af150b3a8ca41e1
ssgayathri <sgayatri@onec1.com>
2025-02-19 00:33:41 +0530

    Update jdbc_source.rb

--- test/policyfiles/jdbc_source.rb
+++ test/policyfiles/jdbc_source.rb
@@ -1,6 +1,7 @@
 name 'pris_jdbc_source'
 default_source :supermarket
-run_list 'pris_java::default', 'pris_test::jdbc_source'
+run_list 'openldap_patch', 'pris_java::default', 'pris_test::jdbc_source'
 cookbook 'pris', path: '../..'
 cookbook 'pris_test', path: '../fixtures/cookbooks/pris_test'
 cookbook 'pris_java', path: '../fixtures/cookbooks/pris_java'
+cookbook 'openldap_patch', path: '../fixtures/cookbooks/openldap_patch'
