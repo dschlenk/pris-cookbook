3431751aa1c450028871e8cde961528c1ccf3c16
ssgayathri <sgayatri@onec1.com>
2025-02-19 00:17:04 +0530

    Update create_if_missing_script_source.rb

--- test/policyfiles/create_if_missing_script_source.rb
+++ test/policyfiles/create_if_missing_script_source.rb
@@ -2,5 +2,5 @@ name 'pris_create_if_missing_script_source'
 default_source :supermarket
 run_list 'pris_java::default', 'pris_test::create_if_missing_script_source'
 cookbook 'pris', path: '../..'
-cookbook 'pris_test', path: '../test/fixtures/cookbooks/pris_test'
-cookbook 'pris_java', path: '../test/fixtures/cookbooks/pris_java'
+cookbook 'pris_test', path: '../fixtures/cookbooks/pris_test'
+cookbook 'pris_java', path: '../fixtures/cookbooks/pris_java'
