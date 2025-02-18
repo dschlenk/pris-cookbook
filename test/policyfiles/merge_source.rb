7369cf3efcac7b68861d9a00fd3595b9a485ed43
ssgayathri <sgayatri@onec1.com>
2025-02-19 00:18:20 +0530

    Update merge_source.rb

--- test/policyfiles/merge_source.rb
+++ test/policyfiles/merge_source.rb
@@ -2,5 +2,5 @@ name 'pris_merge_source'
 default_source :supermarket
 run_list 'pris_java::default', 'pris_test::merge_source'
 cookbook 'pris', path: '../..'
-cookbook 'pris_test', path: '../test/fixtures/cookbooks/pris_test'
-cookbook 'pris_java', path: '../test/fixtures/cookbooks/pris_java'
+cookbook 'pris_test', path: '../fixtures/cookbooks/pris_test'
+cookbook 'pris_java', path: '../fixtures/cookbooks/pris_java'
