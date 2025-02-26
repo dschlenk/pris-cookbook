62cd80c62a198763b1abea3ea0b08644ad842f00
David Schlenk <dschlenk@onec1.com>
2025-02-26 18:55:58 +0000

    branch got merged

--- test/fixtures/cookbooks/openldap_patch/recipes/default.rb
+++ test/fixtures/cookbooks/openldap_patch/recipes/default.rb
@@ -1,5 +1,5 @@
 remote_file "#{Chef::Config['file_cache_path']}/openldap-2.6.6-4.el9.x86_64.rpm" do
-  source 'https://github.com/dschlenk/opennms-cookbook/raw/refs/heads/feature/cinc-18/files/default/openldap-2.6.6-4.el9.x86_64.rpm'
+  source 'https://github.com/dschlenk/opennms-cookbook/raw/refs/heads/master/files/default/openldap-2.6.6-4.el9.x86_64.rpm'
   not_if 'rpm -q openldap-2.6.6-4'
 end
 
